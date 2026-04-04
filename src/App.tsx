701703aeec641acd041945614e3fe7d9ffd40d6e
Peenguino <giuseppe_acocella@outlook.it>
2026-04-04 12:38:50 +0200

    update: adding routing and first table ui version

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,20 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Homepage from './userInterfaceComponents/homePage'
 import PlayingCard from './cardComponents/playingCard'
+import Playingpage from './userInterfaceComponents/playingPage'
 
 function App() {
 
   return (
-    <div style={{ display: 'flex', 
-    flexWrap: 'wrap', // Permette agli elementi di andare a capo
-    gap: '20px', 
-    padding: '10px', 
-    backgroundColor: '#2e7d32', 
-    minHeight: '100vh',
-    minWidth: '100vh',
-
-     }}>
-
-      <PlayingCard rank="1" suit="denari" />
-      <PlayingCard rank="2" suit="denari" />
-      <PlayingCard rank="3" suit="denari" />
-      <PlayingCard rank="4" suit="denari" />
-      <PlayingCard rank="5" suit="denari" />
-      <PlayingCard rank="6" suit="denari" />
-      <PlayingCard rank="7" suit="denari" />
-      <PlayingCard rank="8" suit="denari" />
-      <PlayingCard rank="9" suit="denari" />
-      <PlayingCard rank="10" suit="denari" />
-
-
-      <PlayingCard rank="1" suit="spade" />
-      <PlayingCard rank="2" suit="spade" />
-      <PlayingCard rank="3" suit="spade" />
-      <PlayingCard rank="4" suit="spade" />
-      <PlayingCard rank="5" suit="spade" />
-      <PlayingCard rank="6" suit="spade" />
-      <PlayingCard rank="7" suit="spade" />
-      <PlayingCard rank="8" suit="spade" />
-      <PlayingCard rank="9" suit="spade" />
-      <PlayingCard rank="10" suit="spade" />
-
-
-      <PlayingCard rank="1" suit="bastoni" />
-      <PlayingCard rank="2" suit="bastoni" />
-      <PlayingCard rank="3" suit="bastoni" />
-      <PlayingCard rank="4" suit="bastoni" />
-      <PlayingCard rank="5" suit="bastoni" />
-      <PlayingCard rank="6" suit="bastoni" />
-      <PlayingCard rank="7" suit="bastoni" />
-      <PlayingCard rank="8" suit="bastoni" />
-      <PlayingCard rank="9" suit="bastoni" />
-      <PlayingCard rank="10" suit="bastoni" />
-
-
-      <PlayingCard rank="1" suit="coppe" />
-      <PlayingCard rank="2" suit="coppe" />
-      <PlayingCard rank="3" suit="coppe" />
-      <PlayingCard rank="4" suit="coppe" />
-      <PlayingCard rank="5" suit="coppe" />
-      <PlayingCard rank="6" suit="coppe" />
-      <PlayingCard rank="7" suit="coppe" />
-      <PlayingCard rank="8" suit="coppe" />
-      <PlayingCard rank="9" suit="coppe" />
-      <PlayingCard rank="10" suit="coppe" />
-
-
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage/>} />
+          <Route path='/playingpage' element={<Playingpage/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/userInterfaceComponents/playingPage.tsx
@@ -0,0 +1,13 @@
+import './playingPage.css'
+
+function TableContainer() {
+    return <div className='table-container'>
+        banco
+    </div>
+}
+
+export default function Playingpage() {
+    return <div className='outer-container'>
+        <TableContainer/>
+    </div>
+}
